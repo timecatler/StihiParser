3bee2c29be679e8f81792d07b7fbd5aaabd6d2d8
Tim Catler <user97@ya.ru>
2015-07-24 00:14:08 +0300

    Add PoemParser class for parsing one poem

---
+++ src/main/java/com/stihi/Parser/PoemParser.java
@@ -0,0 +1,54 @@
+package com.stihi.Parser;
+
+import com.stihi.Util.Poem;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+class PoemParser extends CommonParser {
+    Elements _poem = new Elements();
+
+    public Elements parsePoem(String source) throws IOException {
+        // This can throw an exception
+        load(source);
+
+        Elements poem = _doc.select("h1");
+        poem.addAll(_doc.select("div.text"));
+        _poem.addAll(poem);
+        return poem;
+    }
+
+    public void printPoem() throws IllegalStateException {
+        System.out.print(getStringPoem());
+    }
+
+    public String getStringPoem() throws IllegalStateException {
+        if (_poem.isEmpty()) throw new IllegalStateException("No poem");
+        String text = "";
+        for (Element el : _poem) {
+            text += el.toString().replaceAll("<(.*?)>", "").replaceAll("&(.*?);", "") + '\n';
+        }
+        return text;
+    }
+
+    public String getPoemHeader() {
+        if (_poem.isEmpty()) throw new IllegalStateException("No poem");
+        return _poem.first().toString().replaceAll("<(.*?)>", "").replaceAll("&(.*?);", "");
+    }
+
+    public String getPoemText() {
+        if (_poem.isEmpty()) throw new IllegalStateException("No poem");
+        String text = "";
+        for (Element el : _poem) {
+            if (el == _poem.first()) continue;
+            text += el.toString().replaceAll("<(.*?)>", "").replaceAll("&(.*?);", "");
+        }
+        return text;
+    }
+
+    public Poem getPoem() {
+        if (_poem.isEmpty()) throw new IllegalStateException("No poem");
+        return new Poem(getPoemHeader(), getPoemText());
+    }
+}
