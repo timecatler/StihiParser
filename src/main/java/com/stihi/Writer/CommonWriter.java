c0a4a60c98cd28c93b0086ae4c140b8170589683
Tim Catler <user97@ya.ru>
2015-07-24 00:13:14 +0300

    Add CommonWriter class
    Writes directly to file

---
+++ src/main/java/com/stihi/Writer/CommonWriter.java
@@ -0,0 +1,24 @@
+package com.stihi.Writer;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CommonWriter {
+    public void writeStrings(String path, List<String> source) throws IOException {
+        File file = new File(path);
+
+        if (!file.exists()) file.createNewFile();
+
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (String line : source) {
+            bw.write(line);
+            bw.newLine();
+        }
+        bw.close();
+        fw.close();
+    }
+}
